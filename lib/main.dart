8c93a7656f6c1ce0f7ce3e160afc486f4d304250
MinhChanhDev <chanhlm.21it@vku.udn.vn>
2024-08-29 01:48:06 +0700

    I am rich

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const IamRich());
+}
+
+class IamRich extends StatelessWidget {
+  const IamRich({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.amber.shade700,
+          appBar: AppBar(
+            backgroundColor: Colors.amber.shade900,
+            title: const Text("I am Rich",
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage("images/diamond1.png"),
+                  width: 330,
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
